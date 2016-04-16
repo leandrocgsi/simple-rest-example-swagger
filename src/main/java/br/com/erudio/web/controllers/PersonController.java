6fef4d968d6ba270e68de1ecd3ccc0267c06fd53
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-15 22:23:14 -0300

    Adding Swagger documentation configs

--- src/main/java/br/com/erudio/web/controllers/PersonController.java
+++ src/main/java/br/com/erudio/web/controllers/PersonController.java
@@ -13,43 +13,54 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
 import br.com.erudio.models.Person;
 import br.com.erudio.services.PersonService;
 
+
+@Api(value = "person")
 @RestController
 @RequestMapping("/person/")
 public class PersonController {
-    
-    @Autowired
-    private PersonService personService;
-    
+	
+	@Autowired
+	private PersonService personService;
+	
+	@ApiOperation(value = "Find person by ID" )
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/{personId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person get(@PathVariable(value = "personId") String personId){
         return personService.findById(personId);
     }
-    
-    @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
-        return personService.findAll();
-    }
-    
-    @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person){
-        return personService.create(person);
-    }
-    
-    @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person){
-        return personService.update(person);
-    }
+	
+	@ApiOperation(value = "Find all persons" )
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return personService.findAll();
+	}
+	
+	@ApiOperation(value = "Create a new person" )
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+		return personService.create(person);
+	}
+	
+	@ApiOperation(value = "Update an existing person")
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person){
+		return personService.update(person);
+	}
 
-    @ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Delete person by ID" )
+	@ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/{personId}", method = RequestMethod.DELETE)
     public void delete(@PathVariable(value = "personId") String personId){
         personService.delete(personId);
     }
+	
 }
